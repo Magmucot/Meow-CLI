3374a931ac36ef85ec3e5b5d8857d5f63b79a2fd
cons0leweb <cons0leweb@users.noreply.github.com>
2026-04-12 21:59:41 +0300

    patch test_ui.js

--- test_ui.js
+++ test_ui.js
@@ -8,6 +8,7 @@ const color = (hex) => {
   return new Proxy(wrapper, {
     get(target, prop) {
       if (prop === 'hexCode') return hex;
+      if (prop === 'toString') return () => fn.open || "";
       if (prop === Symbol.toPrimitive) return (hint) => (hint === 'number') ? null : (fn.open || "");
       const val = fn[prop];
       return typeof val === 'function' ? val.bind(fn) : val;

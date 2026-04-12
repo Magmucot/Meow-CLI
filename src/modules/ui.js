ebc1c96ea64498894cc36fe310eaf2a604aec932
cons0leweb <cons0leweb@users.noreply.github.com>
2026-04-12 22:00:48 +0300

    patch src/modules/ui.js

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -5,13 +5,15 @@ import chalk from "chalk";
 import boxen from "boxen";
 import gradient from "gradient-string";
 
+
 const color = (hex) => {
   const fn = chalk.hex(hex);
   const wrapper = (text) => fn(text);
   return new Proxy(wrapper, {
     get(target, prop) {
       if (prop === 'hexCode') return hex;
-      if (prop === 'toString') return () => fn.open;
+      if (prop === 'toString') return () => fn.open || "";
+      if (prop === Symbol.toPrimitive) return (hint) => (hint === 'number' ? null : (fn.open || ""));
       const val = fn[prop];
       return typeof val === 'function' ? val.bind(fn) : val;
     }
